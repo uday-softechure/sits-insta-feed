2d03bbac5982ca659304e85302d932cd6e3fa8c3
uday <uday.softechure@gmail.com>
2024-07-22 16:50:29 +0530

    vendor remove

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Package View</title>
+</head>
+<body>
+    <h1>Hello from the package view!</h1>
+</body>
+</html>
---
+++ src/routes/web.php
@@ -0,0 +1,3 @@
+Route::get('package', function() {
+    return 'Hello from the package!';
+});
