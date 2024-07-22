82e81c55a3f65f47bfb0acc8c47f909dee55e926
uday <uday.softechure@gmail.com>
2024-07-22 16:57:08 +0530

    vendor remove

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,3 +1,5 @@
-Route::get('package', function() {
-    return 'Hello from the package!';
+<?php
+Route::get('sits-insta-feed', function() {
+    // return 'Hello from the package!';
+    return view('sits-insta-feed::welcome');
 });
